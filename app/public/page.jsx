36c642e4a3482169287560968b79bf8ad7cf71cf
Amirhossein Mirshahi <amirmsh@student.ubc.ca>
2024-02-07 23:43:18 -0800

    practice box model in css with buttons - added more buttons

--- app/public/page.jsx
+++ app/public/page.jsx
@@ -1,36 +1,94 @@
 const Public = () => {
-  return <section className="bg-gray-100 p-12">
-    <div className="mb-6 bg-emerald-400 w-[45em] mx-10 border-y-4 px-3 py-4">Hello this is my dive that I want to play with its box model.</div>
-    <div className="bg-indigo-700 w-[34em] border-y-[32px] mt-1 px-9 py-3 mb-8">Here is some neighbor to it, to check a few things. Lool!</div>
+  const page_nums = [1, 2, 3, 4, 5];
 
+  return (
+    <section className="bg-gray-100 p-12">
+      <div className="mb-6 bg-emerald-400 w-[45em] mx-10 border-y-4 px-3 py-4">
+        Hello this is my dive that I want to play with its box model.
+      </div>
+      <div className="bg-indigo-700 w-[34em] border-y-[32px] mt-1 px-9 py-3 mb-8">
+        Here is some neighbor to it, to check a few things. Lool!
+      </div>
 
-    <button className="bg-black w-[130px] h-[50px]
+      <button
+        className="bg-black px-4 py-3
       hover:opacity-80 transition
-    ">Request Now</button>
-    <button className="bg-yellow-400 text-black mx-4 w-[200px] h-[50px] rounded-full
+    "
+      >
+        Request Now
+      </button>
+      <button
+        className="bg-yellow-400 text-black mx-4 px-14 py-3 rounded-full
       hover:bg-yellow-500 transition
-    ">Add to Cart</button>
-    <button className="w-[100px] h-[50px] font-bold bg-emerald-600 rounded-xl
+    "
+      >
+        Add to Cart
+      </button>
+      <button
+        className="py-3 px-4 font-bold bg-emerald-600 rounded-xl
       hover:shadow-lg transition
-    ">Sign up</button>
-    <button className="bg-violet-700 w-[8rem] h-[3rem] mx-4 rounded-[5px]
+    "
+      >
+        Sign up
+      </button>
+      <button
+        className="bg-violet-700 py-3 px-5 mx-4 rounded-[5px]
       hover:opacity-80 transition font-semibold
-    ">Get Started</button>
-    <button className="text-gray-500 w-[8rem] h-[3rem] border border-slate-500 rounded mt-3
+    "
+      >
+        Get Started
+      </button>
+      <button
+        className="text-gray-500 py-3 px-5 border border-slate-500 rounded mt-3
       hover:bg-gray-500 hover:text-white transition font-semibold
-    ">Download</button>
-    <br/>
-    <button className="bg-blue-700 w-[230px] h-[40px] mt-5 rounded-full font-semibold
+    "
+      >
+        Download
+      </button>
+      <br />
+      <button
+        className="py-3 px-4 bg-blue-700 mt-5 rounded-full font-semibold
       hover:bg-blue-900 transition
-    ">
-      Apply on company website
-    </button>
-    <button className="w-[90px] h-[50px] border border-blue-700 text-blue-700 font-bold mx-4
+    "
+      >
+        Apply on company website
+      </button>
+      <button
+        className="py-3 px-6 border border-blue-700 text-blue-700 font-bold mx-4
       rounded-full hover:border-2 hover:bg-blue-100 transition
-    ">
-      Save
-    </button>
-  </section>
+    "
+      >
+        Save
+      </button>
+
+      <br />
+
+      <button className="py-2 px-3 bg-gray-200 border border-slate-600 text-black rounded my-4 mr-3">
+        Back
+      </button>
+      {page_nums.map((num, index) => (
+        <a key={index} href="" className="text-black mx-1">
+          {num}
+        </a>
+      ))}
+      <button className="py-2 px-3 bg-gray-200 border border-slate-600 text-black rounded ml-3 mr-4">
+        Next
+      </button>
+      <br />
+
+      <button className="bg-emerald-600 py-3 px-4 hover:py-6 hover:px-8 transition-all duration-1000">
+        Stretch
+      </button>
+      <button className="bg-emerald-600 py-3 px-4 shadow-lg mx-4 active:translate-x-1 active:translate-y-1 active:shadow-none">
+        Shadow
+      </button>
+
+      <button className="bg-emerald-600 py-3 px-4 hover:px-5 transition-all mx-3">One</button>
+      <button className="bg-emerald-600 py-3 px-4 hover:px-6 transition-all mx-3">Two</button>
+      <button className="bg-emerald-600 py-3 px-4 hover:px-7 transition-all mx-3">Three</button>
+
+    </section>
+  );
 };
 
 export default Public;

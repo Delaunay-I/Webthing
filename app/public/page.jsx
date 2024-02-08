b1407061213ab284a2aa60744b843082d1cc5429
Amirhossein <96101841+Delaunay-I@users.noreply.github.com>
2024-02-08 04:48:07 +0000

    Added some buttons

--- app/public/page.jsx
+++ app/public/page.jsx
@@ -1,5 +1,13 @@
 const Public = () => {
-  return <div>Public</div>;
+  return <section className="bg-gray-100 p-12">
+    <div className="mb-6 bg-emerald-400 w-[45em] mx-10 border-y-4 px-3 py-4">Hello this is my dive that I want to play with its box model.</div>
+    <div className="bg-indigo-700 w-[34em] border-y-[32px] mt-1 px-9 py-3 mb-8">Here is some neighbor to it, to check a few things. Lool!</div>
+    <button className="bg-black w-[130px] h-[50px]">Request Now</button>
+    <button className="bg-yellow-400 text-black mx-4 w-[200px] h-[50px] rounded-full">Add to Cart</button>
+    <button className="w-[100px] h-[50px] bg-emerald-600 rounded-xl">Sign up</button>
+    <button className="bg-violet-600 w-[8rem] h-[3rem] mx-4 rounded-[5px]">Get Started</button>
+    <button className="text-gray-500 w-[8rem] h-[3rem] border border-slate-500 rounded mt-3">Download</button>
+  </section>
 };
 
 export default Public;

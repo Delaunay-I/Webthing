26eb9c4ec8aa646e249d7b23982e7d927975746e
Amirhossein <96101841+Delaunay-I@users.noreply.github.com>
2024-02-19 08:36:19 +0000

    changes to the home buttons

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,31 @@
 import Link from "next/link";
 
 const Home = () => {
-  return <section>
-    <p className="mb-4 text-3xl font-semibold">Sections of this website:</p>
+  return (
+    <section>
+      <p className="mb-4 text-3xl font-semibold">Sections of this website:</p>
 
-<div className="grid grid-cols-3 gap-2">
-    <button className="bg-amber-600 px-3 py-3 rounded-2xl shadow-lg
-      hover:opacity-80  text-white font-bold
-      transition active:translate-x-[2px] active:translate-y-[2px]
-    "><Link href="/public/layouts">Layouts</Link></button>
-    <button className="bg-amber-600 px-3 py-3 rounded-2xl shadow-lg
+      <div className="grid grid-cols-3 gap-2">
+        <Link
+          href="/public/layouts"
+          className="bg-amber-600 px-3 py-3 rounded-2xl shadow-lg
+        hover:opacity-80  text-white font-bold
+        transition"
+        >
+          Layouts
+        </Link>
+
+        <Link
+          href="/public/grid"
+          className="bg-amber-600 px-3 py-3 rounded-2xl shadow-lg
       hover:opacity-80  text-white font-bold 
-      transition active:translate-x-[2px] active:translate-y-[2px]
-    "><Link href="/public/grid">Grid</Link></button>
-</div>
-  </section>
-}
+      transition"
+        >
+          Grid
+        </Link>
+      </div>
+    </section>
+  );
+};
 
-
-export default Home; 
+export default Home;
